7fbc7e8527e0d04f2d4b8c7ccba1b6ebd7ca057e
Terence Lee <hone02@gmail.com>
2018-09-03 00:11:14 -0500

    initial commit

--- test/palm-island-test.ts
+++ test/palm-island-test.ts
@@ -1,7 +1,28 @@
-import hello from 'palm-island';
+import { Area, Card, Resource, TEMPLATES, flip, rotate } from 'palm-island';
+import { expect } from 'ts-std';
 
 QUnit.module('palm-island tests');
 
-QUnit.test('hello', assert => {
-  assert.equal(hello(), 'Hello from palm-island');
+QUnit.test('rotate', assert => {
+  assert.equal(rotate(Area.FrontTop), Area.FrontBottom);
+  assert.equal(rotate(Area.FrontBottom), Area.FrontTop);
+  assert.equal(rotate(Area.BackTop), Area.BackBottom);
+  assert.equal(rotate(Area.BackBottom), Area.BackTop);
+});
+
+QUnit.test('flip', assert => {
+  assert.equal(flip(Area.FrontTop), Area.BackTop);
+  assert.equal(flip(Area.FrontBottom), Area.BackBottom);
+  assert.equal(flip(Area.BackTop), Area.FrontTop);
+  assert.equal(flip(Area.BackBottom), Area.FrontBottom);
+});
+
+QUnit.test('card', assert => {
+  let canoeHouse = expect(TEMPLATES.find(t => t.name === 'Canoe House'));
+  let card = new Card(canoeHouse);
+
+  assert.equal(card.name, 'Canoe House');
+  assert.deepEqual(card.resources, [Resource.Fish]);
+  assert.equal(card.canRotate, true);
+  assert.equal(card.canFlip, true);
 });
